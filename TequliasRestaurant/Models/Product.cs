e9ba1a271f46811b07621cd2e7c846e9589682fb
omarelnahs <omarelnahas1141@gmail.com>
2025-07-06 13:00:05 +0300

    Add project files.

---
+++ TequliasRestaurant/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TequliasRestaurant.Models
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public int CategoryId { get; set; }
+
+        [NotMapped]
+        public IFormFile? ImageFile { get; set; }
+        public string ImageUrl { get; set; } = "https://via.placeholder.com/150";
+
+        [ValidateNever]
+        public Category? Category { get; set; } // product belongs to category
+
+        [ValidateNever]
+        public ICollection<OrderItem>? OrderItems { get; set; } // product can be in many order items
+        
+        [ValidateNever]
+        public ICollection<ProductIngredient>? ProductIngredients { get; set; } // product can have many ingredients
+    }
+}
