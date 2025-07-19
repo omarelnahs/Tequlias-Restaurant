ca924c8495350ca237b0198bc1d1e0826e16034d
omarelnahs <omarelnahas1141@gmail.com>
2025-07-19 22:23:39 +0300

    Update product management and navigation links
    - Revamped navigation menu in `_Layout.cshtml` to include "Ingredient" and "Menu Items" links.
    - Added `.fixed-image-height` class in `site.css` for consistent image display.
    - Deleted two binary image files: `veggietacos.jpg`.
    - Created `ProductController` with methods for listing and managing products.
    - Introduced `AddEdit.cshtml` for product creation/editing with relevant fields.
    - Updated `Index.cshtml` to display product list with create and delete options.

---
+++ TequliasRestaurant/Controllers/ProductController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TequliasRestaurant.Data;
+using TequliasRestaurant.Models;
+
+namespace TequliasRestaurant.Controllers
+{
+    public class ProductController : Controller
+    {
+        private Repository<Product> products;
+        private Repository<Ingredient> ingredients;
+        private Repository<Category> categories;
+
+        public ProductController(ApplicationDbContext context)
+        {
+            products = new Repository<Product>(context);
+            ingredients = new Repository<Ingredient>(context);
+            categories = new Repository<Category>(context);
+        }
+
+        // GET: Product
+        public async Task<IActionResult> Index()
+        {
+            return View(await products.GetAllAsync());
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddEdit(int id)
+        {
+            ViewBag.Ingredients = await ingredients.GetAllAsync();
+            ViewBag.Categories = await categories.GetAllAsync();
+            if (id == 0)
+            {
+                ViewBag.Operation = "Add";
+                return View(new Product());
+            }
+            else
+            {
+                ViewBag.Operation = "Edit";
+                return View();
+            }
+            
+        }
+    }
+}
